d91f76c496c886386d6af1a9e1e3093a39696761
Developer (STED) <mattia.donato@iit.it>
2023-05-19 17:15:32 +0200

    Added the minimum packet

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main(){
-    println!("cargo:rustc-link-lib=dylib=nifpga");
-    println!("cargo:rustc-link-search=native=./");
-}
-
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@ use std::path::PathBuf;
 use std::process;
 use std::time::Duration;
 
-use std::thread;
-use std::thread::available_parallelism;
-use std::sync::mpsc;
-use std::sync::mpsc::SyncSender;
+//use std::thread;
+//use std::thread::available_parallelism;
+//use std::sync::mpsc;
+//use std::sync::mpsc::SyncSender;
+use std::time::Instant;
 
+use crossbeam;
 
 use clap::{Parser};
 use nifpga::{NifpgaError, Session, ReadFifo, WriteFifo, ReadElements};
-
 
 
 #[derive(Parser)]
@@ -46,6 +47,10 @@ struct Cli {
 
     #[arg(short, long, value_name = "13123",  default_value = "13123")]
     port: Option<usize>,
+
+    #[arg(short, long, value_name = "1",  default_value = "1")]
+    min_packet: Option<usize>,
+
 }
 
 #[derive(Clone)]
@@ -59,6 +64,7 @@ struct Configuration {
     port: usize,
     dma_buffer_size: usize,
     fifo_reading_buffer: usize,
+    min_packet: usize
 }
 
 impl Configuration{
@@ -73,6 +79,7 @@ impl Configuration{
             port: 0,
             dma_buffer_size: 0,
             fifo_reading_buffer: 0,
+            min_packet:1
         }
     }
 }
@@ -130,10 +137,15 @@ fn import_args_as_configuration() -> Configuration{
         println!("fifo_reading_buffer: {}", conf.fifo_reading_buffer);
     }
 
+    if let Some(min_packet) = cli.min_packet {
+        conf.min_packet = min_packet;
+        println!("min_packet: {}", conf.min_packet);
+    }
+
     conf
 }
 
-fn zmq_loop(mut conf: &Configuration, rx:std::sync::mpsc::Receiver<Vec<u64>>) {
+fn zmq_loop(mut conf: &Configuration, rx: crossbeam::channel::Receiver<Vec<u64>>) {
     let ctx = zmq::Context::new();
     let socket = ctx.socket(zmq::PUSH).unwrap();
     let tcp_string = "tcp://127.0.0.1:".to_string() + &conf.port.to_string();
@@ -145,7 +157,6 @@ fn zmq_loop(mut conf: &Configuration, rx:std::sync::mpsc::Receiver<Vec<u64>>) {
         // println!("rx.recv()");
         match rx.recv() {
             Ok(chunk) => {
-
                 let (head, chunk_u8, tail) = unsafe { chunk.align_to::<u8>() };
                 assert!(head.is_empty()); //paranoid check that the previous call was not messing up
                 assert!(tail.is_empty());
@@ -154,7 +165,7 @@ fn zmq_loop(mut conf: &Configuration, rx:std::sync::mpsc::Receiver<Vec<u64>>) {
                 if chunk.len()>0 {
                     match socket.send(chunk_u8, 0) {
                         Ok(T) => {
-                            println!("{} {}", chunk.len(), chunk_u8.len());
+                            //println!("tx: {}", chunk.len());
                         }
                         Err(E) => {
                             println!("ERR {:?}", E);
@@ -171,7 +182,7 @@ fn zmq_loop(mut conf: &Configuration, rx:std::sync::mpsc::Receiver<Vec<u64>>) {
     }
 }
 
-fn fpga_loop(mut conf: &Configuration, tx:std::sync::mpsc::SyncSender<Vec<u64>>) -> Result<(), NifpgaError> {
+fn fpga_loop(mut conf: &Configuration, tx: crossbeam::channel::Sender<Vec<u64>>) -> Result<(), NifpgaError> {
 
     let session = Session::open(
         conf.bit_file.as_str(),
@@ -183,22 +194,36 @@ fn fpga_loop(mut conf: &Configuration, tx:std::sync::mpsc::SyncSender<Vec<u64>>)
 
     let (reader, depth) = session.open_read_fifo::<u64>(conf.fifo, conf.dma_buffer_size)?;
 
-    println!("Actual FIFO {} set depth: {} actual depth: {}", conf.fifo, conf.dma_buffer_size, depth);
+    println!("Actual DMA FIFO {} set depth: {} actual depth: {}", conf.fifo, conf.dma_buffer_size, depth);
     println!("conf.fifo_reading_buffer: {}", conf.fifo_reading_buffer);
 
     let mut read_buff:Vec<u64> = Vec::with_capacity(conf.fifo_reading_buffer);
     read_buff.resize(conf.fifo_reading_buffer, 0);
 
     let mut read_buff_zero_size:Vec<u64> = Vec::with_capacity(0);
+    let mut data_available=0;
+
+    let mut now_time = Instant::now();
 
     loop {
-        let data_available = reader.read(&mut read_buff_zero_size, 0)?;
-        if (data_available>0) {
-            read_buff.resize(data_available, 0);
-            let r = reader.read(&mut read_buff, 0)?;
-            //println!("r {:?} {:?}", data_available, r);
-            tx.send(read_buff.to_vec());
+        let last_time = Instant::now();
+        if data_available==0 {
+            data_available = (reader.read(&mut read_buff_zero_size, 0)? / conf.min_packet)*conf.min_packet;
         }
+
+        if data_available>0 {
+            //println!("f:{}", data_available);
+            read_buff.resize(data_available, 0);
+            data_available = (reader.read(&mut read_buff, 10000)? / conf.min_packet)*conf.min_packet ;
+            tx.send(read_buff.to_vec()).unwrap();
+        }
+        if data_available>0 {
+            //println!("r {:?}", data_available);
+        }
+        now_time = Instant::now();
+        let delta_time
+            = now_time - last_time;
+
     }
 
     Ok(())
@@ -210,15 +235,20 @@ fn fpga_loop(mut conf: &Configuration, tx:std::sync::mpsc::SyncSender<Vec<u64>>)
 fn main() -> Result<(), NifpgaError>{
     let mut conf = import_args_as_configuration();
     let mut conf2 = conf.clone();
-    let channel_buffer_size = 10000;
+    // const channel_buffer_size = 10000;
 
-    let (tx,rx) = mpsc::sync_channel(channel_buffer_size);
+    let (tx,rx) = crossbeam::channel::unbounded();
 
-    let t1= thread::spawn(move || {
-            let _ = zmq_loop(&conf2.clone(),rx);
-    });
+    println!("A");
 
-    fpga_loop(&conf,tx)
+    crossbeam::thread::scope( |s| {
+        let t1 = s.spawn(move |_| zmq_loop(&conf2.clone(),rx));
+        let t2 = s.spawn(move |_| fpga_loop(&conf,tx));
+    }).unwrap();
+
+    println!("B");
+
+    Ok(())
 }
 
 
